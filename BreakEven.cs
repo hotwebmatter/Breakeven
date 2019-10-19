8ab7a64d0c3e19c29bf094277ece400ecacc029e
Matt Obert on Windows <matt@hotwebmatter.com>
2019-10-19 19:01:07 -0400

    Create BreakEven class with private data members, setters

---
+++ BreakEven.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Breakeven
+{
+    class BreakEven
+    {
+        string companyName;
+        double sellingPrice;
+        double fixedOverhead;
+        double adminSalaries;
+        double materialsPercentage;
+        double laborPercentage;
+        double contributionMargin;
+        double breakevenUnits;
+        double breakevenDollars;
+
+        public BreakEven()
+        {
+            // empty constructor, per specification
+        }
+
+        // properties -- accessors and mutators
+
+        public string CompanyName
+        {
+            set // mutator method
+            {
+                companyName = value;
+            }
+        }
+        public double SellingPrice
+        {
+            set // mutator method
+            {
+                sellingPrice = value;
+            }
+        }
+        public double AdminSalaries
+        {
+            set // mutator method
+            {
+                adminSalaries = value;
+            }
+        }
+        public double MaterialsPercentage
+        {
+            set // mutator method
+            {
+                materialsPercentage = value;
+            }
+        }
+        public double LaborPercentage
+        {
+            set // mutator method
+            {
+                laborPercentage = value;
+            }
+        }
+    }
+}
