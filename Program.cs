13d7abc8bec497a7bde8d434f585f25ebcbb8861
Matt Obert on Windows <matt@hotwebmatter.com>
2019-10-19 20:18:17 -0400

    Ask user for some values

--- Program.cs
+++ Program.cs
@@ -14,7 +14,34 @@ namespace Breakeven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputValue;
+            string name;
+            double price;
+            double overhead;
+            double salaries;
+            double materials;
+            double labor;
+            Write("Enter company name: ");
+            name = ReadLine();
+            Write("Enter selling price: ");
+            inputValue = ReadLine();
+            price = double.Parse(inputValue);
+            Write("Enter fixed overhead: ");
+            inputValue = ReadLine();
+            overhead = double.Parse(inputValue);
+            Write("Enter fixed administrative salaries: ");
+            inputValue = ReadLine();
+            salaries = double.Parse(inputValue);
+            Write("Enter raw materials percent of selling price: ");
+            inputValue = ReadLine();
+            materials = double.Parse(inputValue);
+            Write("Enter labor cost percent of selling price: ");
+            inputValue = ReadLine();
+            labor = double.Parse(inputValue);
+
+            // instantiate object by calling BreakEven constructor method
+            BreakEven calculation = new BreakEven();
+
         }
     }
 }
